69f805d46c4eb4dcc347964f367fa01ee9d82894
FM Young <fyimail@gmail.com>
2023-07-29 17:21:10 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import openai
+import streamlit as st
+import pprint 
+
+# add custom CSS
+st.markdown("""
+<style>
+    .stApp, .css-90vs21 {
+        background-color: #d7ffd7;
+    }
+
+</style>
+""", unsafe_allow_html=True)
+st.title("ChatGPT-like ChatBot")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+print(st.session_state)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    print(f"LOG: {st.session_state}")
